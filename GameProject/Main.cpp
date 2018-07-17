5a837469d4c2c20b75fead41fc6434a7681a561a
Exaious <exaious@gmail.com>
2018-07-17 10:57:36 -0700

    Did all the stuff(GameLoop)

--- GameProject/Main.cpp
+++ GameProject/Main.cpp
@@ -5,20 +5,22 @@ const int TICKS_PER_SECOND = 100;
 const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
 const int MAX_FRAMESKIP = 5;
 
+void GameLogic() {
+	std::cout << "Hammer is a legend" << std::endl;
+}
+
 int main(int argc, char * argv[]){
-	Uint32 next_game_tick = SDL_GetTicks();
+	Uint32 gameTick = SDL_GetTicks();
 	int loops;
-	float interpolation;
 
-	bool game_is_running = true;
-	while (game_is_running) {
 
+	bool inGame = true;
+	while (inGame) {
 		loops = 0;
-		while (SDL_GetTicks() > next_game_tick && loops < MAX_FRAMESKIP) {
-			next_game_tick += SKIP_TICKS;
+		while (SDL_GetTicks() > gameTick && loops < MAX_FRAMESKIP) {
+			gameTick += SKIP_TICKS;
 			loops++;
-
-			std::cout << "Frame" << std::endl;
+			GameLogic();
 		}
 	}
 	return 0;
