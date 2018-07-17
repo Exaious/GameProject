82cf5e2864925a6d7d963f8834b042e409b79ae7
DarkSpark0905 <DarkSpark0905@gmail.com>
2018-07-17 14:27:58 -0700

    monkaS

--- GameProject/Main.cpp
+++ GameProject/Main.cpp
@@ -1,45 +1,72 @@
-#include <iostream>
 #include <SDL.h>
+#include <iostream>
+
 #include "TextureLoading.h"
 
 const int TICKS_PER_SECOND = 100;
 const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
 const int MAX_FRAMESKIP = 5;
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+SDL_Window * window = NULL;
+SDL_Renderer * renderer = NULL;
 
-//The window we'll be rendering to
-SDL_Window* window = NULL;
 
-//The surface contained by the window
-SDL_Surface* screenSurface = NULL;
-
-void GameLogic() {
-	std::cout << "Hammer is a legend" << std::endl;
-}
-
-int main(int argc, char * argv[]){
+bool init()
+{
+	//Initialization flag
+	bool success = true;
 
 	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO) < 0 || IMG_Init(IMG_INIT_PNG) < 0)
 	{
-		//printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		}
 	}
 
+	return success;
+}
 
-	Uint32 gameTick = SDL_GetTicks();
+void GameLogic() {
+
+}
+
+int main(int argc, char * argv[]) {
+	init();
+	SDL_Texture * texture = GenerateTexture("monkaS", renderer);
+	Uint32 gameTicks = SDL_GetTicks();
 	int loops;
-
 
 	bool inGame = true;
 	while (inGame) {
 		loops = 0;
-		while (SDL_GetTicks() > gameTick && loops < MAX_FRAMESKIP) {
-			gameTick += SKIP_TICKS;
+		while (SDL_GetTicks() > gameTicks && loops < MAX_FRAMESKIP) {
+			gameTicks += SKIP_TICKS;
 			loops++;
 			GameLogic();
+
+			SDL_RenderClear(renderer);
+			SDL_RenderCopy(renderer, texture, NULL, NULL);
+			SDL_RenderPresent(renderer);
+		}
+		if (loops > 1) {
+			std::cout << "Lag Frames: " << loops - 1 << std::endl;
 		}
 	}
 	return 0;
