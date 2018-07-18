c8271ea567eed18d0e18b0b004b4f9ace6c1edd1
DarkSpark0905 <DarkSpark0905@gmail.com>
2018-07-18 11:11:06 -0700

    Everything

--- GameProject/Main.cpp
+++ GameProject/Main.cpp
@@ -3,6 +3,9 @@
 
 #include "TextureLoading.h"
 
+const int screenX = 1000;
+const int screenY = 800;
+
 const int TICKS_PER_SECOND = 100;
 const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
 const int MAX_FRAMESKIP = 5;
@@ -10,6 +13,21 @@ const int MAX_FRAMESKIP = 5;
 SDL_Window * window = NULL;
 SDL_Renderer * renderer = NULL;
 
+struct Vector {
+	float x = 0, y = 0;
+};
+struct Player {
+	SDL_Rect rect = { 100,100,50,50 };
+	SDL_Texture * sprite = NULL;
+	Vector vel;
+};
+struct Controller {
+	bool right = false;
+	bool left = false;
+
+	bool up = false;
+	bool down = false;
+};
 
 bool init()
 {
@@ -26,7 +44,7 @@ bool init()
 	{
 		//Create window
 		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED, 600, 600, SDL_WINDOW_SHOWN);
+			SDL_WINDOWPOS_UNDEFINED, screenX, screenY, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -42,27 +60,123 @@ bool init()
 
 	return success;
 }
+int Input(SDL_Event * e, Controller * c) {
+	while (SDL_PollEvent(e) != 0)
+	{
+		if (e->type == SDL_QUIT)
+		{
+			return -1;
+		}
+		//User presses a key
+		else if (e->type == SDL_KEYDOWN)
+		{
+			//Select surfaces based on key press
+			switch (e->key.keysym.sym)
+			{
+			case SDLK_LEFT:
+				c->left = true;
+				return 1;
 
-void GameLogic() {
+			case SDLK_RIGHT:
+				c->right = true;
+				return 1;
 
+			case SDLK_UP:
+				c->up = true;
+				return 1;
+
+			case SDLK_DOWN:
+				c->down = true;
+				return 1;
+
+			default:
+				return 0;
+			}
+		}
+		else if (e->type == SDL_KEYUP) {
+			switch (e->key.keysym.sym)
+			{
+			case SDLK_LEFT:
+				c->left = false;
+				return 2;
+
+			case SDLK_RIGHT:
+				c->right = false;
+				return 2;
+
+			case SDLK_UP:
+				c->up = false;
+				return 2;
+
+			case SDLK_DOWN:
+				c->down = false;
+				return 2;
+
+			default:
+				return 0;
+			}
+		}
+	}
+	return -2;
+}
+bool GameLogic(Player * p, Controller * c, int f) {
+	if (f == -1) {
+		return false;
+	}
+	if (p->vel.y > 0) {
+		p->vel.y -= .05f;
+		p->rect.y += (int)p->vel.y;
+	}
+	else {
+		p->vel.y += .05f;
+		p->rect.y += (int)p->vel.y;
+	}
+	if (c->up) {
+		p->vel.y -= .2f;
+	}
+	else if (c->down) {
+		p->vel.y += .2f;
+	}
+	if (p->vel.x > 0) {
+		p->vel.x -= .05f;
+		p->rect.x += (int)p->vel.x;
+	}
+	else {
+		p->vel.x += .05f;
+		p->rect.x += (int)p->vel.x;
+	}
+	if (c->left) {
+		p->vel.x -= .2f;
+	}
+	else if (c->right) {
+		p->vel.x += .2f;
+	}
+	return true;
 }
 
 int main(int argc, char * argv[]) {
 	init();
-	SDL_Texture * texture = GenerateTexture("monkaS", renderer);
+
+	Player player;
+	Controller controls;
+	player.sprite = GenerateTexture("monkaS", renderer);
+
 	Uint32 gameTicks = SDL_GetTicks();
 	int loops;
 
+	SDL_Event e;
 	bool inGame = true;
 	while (inGame) {
 		loops = 0;
 		while (SDL_GetTicks() > gameTicks && loops < MAX_FRAMESKIP) {
 			gameTicks += SKIP_TICKS;
 			loops++;
-			GameLogic();
+			int val = Input(&e, &controls);
+			inGame = GameLogic(&player, &controls, val);
+			if (inGame == false) { break; }
 
 			SDL_RenderClear(renderer);
-			SDL_RenderCopy(renderer, texture, NULL, NULL);
+			SDL_RenderCopy(renderer, player.sprite, NULL, &player.rect);
 			SDL_RenderPresent(renderer);
 		}
 		if (loops > 1) {
