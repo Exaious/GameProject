66884cb028ec007e8cfff5f84cbb894a805c86cb
DarkSpark0905 <DarkSpark0905@gmail.com>
2018-07-18 11:16:16 -0700

    Assets folder added

--- GameProject/TextureLoading.cpp
+++ GameProject/TextureLoading.cpp
@@ -2,7 +2,7 @@
 SDL_Texture * GenerateTexture(std::string path, SDL_Renderer * renderer)
 {
 	SDL_Texture* newTexture = NULL;
-	SDL_Surface* loadedSurface = IMG_Load((path + ".png").c_str());
+	SDL_Surface* loadedSurface = IMG_Load(("Assets/" + path + ".png").c_str());
 	if (loadedSurface == NULL) { std::cout << "Error0" << std::endl; }
 	else {
 		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
