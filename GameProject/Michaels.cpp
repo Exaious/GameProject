a6dab5836126a7ce1ac35126c3bfb49d69e89ff3
Exaious <exaious@gmail.com>
2018-07-19 16:40:33 -0700

    yeete

--- GameProject/Michaels.cpp
+++ GameProject/Michaels.cpp
@@ -5,3 +5,35 @@
 #include "TextureLoading.h"
 
 
+void pMove(Player * p, Controller * c) {
+	if (p->vel.y > 0) {
+		p->vel.y -= .05f;
+		p->rect.y += (int)p->vel.y;
+	}
+	else {
+		p->vel.y += .05f;
+		p->rect.y += (int)p->vel.y;
+	}
+	if (c->up) {
+		p->vel.y -= .2f;
+	}
+	else if (c->down) {
+		p->vel.y += .2f;
+	}
+	if (p->vel.x > 0) {
+		p->vel.x -= .05f;
+		p->rect.x += (int)p->vel.x;
+		background.x += (int)p->vel.x;
+	}
+	else {
+		p->vel.x += .05f;
+		p->rect.x += (int)p->vel.x;
+		background.x += (int)p->vel.x;
+	}
+	if (c->left) {
+		p->vel.x -= .2f;
+	}
+	else if (c->right) {
+		p->vel.x += .2f;
+	}
+}
